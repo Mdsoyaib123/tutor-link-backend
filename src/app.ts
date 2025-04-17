13beeb0a282acc19b1d45a0934c1805d537a58ef
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-04-18 00:07:18 +0600

    create user

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import cors from "cors";
 import express, { Application } from "express";
+import router from "./app/routes";
 
 const app: Application = express();
 app.use(express.json());
 
 app.use(cors());
+app.use('/api/v1', router);
 
 app.get("/", (req, res) => {
   res.send("Welcome to tutor link ");
