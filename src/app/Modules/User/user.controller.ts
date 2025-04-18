4041ba18c314917bcaeec15cb06b53c6e19b7301
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-04-18 15:02:38 +0600

    created user and subject modules

--- src/app/Modules/User/user.controller.ts
+++ src/app/Modules/User/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { createUserSchema } from "./user.validation";
-import {  userService } from "./user.service";
+import { userService } from "./user.service";
 import catchAsync from "../../utilis/catchAsync";
 import sendResponse from "../../utilis/sendResponse";
 
@@ -16,7 +16,66 @@ export const createUser = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
+const getAllUsers = catchAsync(async (req: Request, res: Response) => {
+  const result = await userService.getAllUsers();
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "All user get successfully",
+    data: result,
+  });
+});
+
+const getSingleUser = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await userService.getSingleUser(id);
+  if (!result) {
+    return res.status(404).json({ success: false, message: "User not found" });
+  }
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "single user get successfully",
+    data: result,
+  });
+});
+
+const updateUser = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const result = await userService.updateUser(id, req.body);
+  if (!result) {
+    return res.status(404).json({ success: false, message: "User not found" });
+  }
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: " user updated successfully",
+    data: result,
+  });
+});
+
+const deleteUser = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await userService.deleteUser(id);
+
+  if (!result) {
+    return res.status(404).json({ success: false, message: "User not found" });
+  }
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: " user deleted successfully",
+    data: result,
+  });
+});
 
 export const userController = {
   createUser,
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser
 };
--- src/app/Modules/User/user.routes.ts
+++ src/app/Modules/User/user.routes.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { userController } from "./user.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { userValidationSchema } from "./user.validation";
 
 const router = express.Router();
 
-router.post("/createUser", userController.createUser);
+router.post("/createUser",validateRequest(userValidationSchema.createUserSchema), userController.createUser);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getSingleUser);
+router.patch("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 export const userRoute = router;
