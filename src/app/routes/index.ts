e1b569fbb02af45b9f9c8f2233f9a218a22b9a09
saifulislam106 <saifulislam106915@gmail.com>
2025-04-22 23:55:49 +0600

    review route added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { UserRoutes } from '../modules/user/user.route';
 import { AuthRoutes } from '../modules/auth/auth.route';
 import { PaymentRouter } from '../modules/payment/payment.route';
+import { ReviewRoutes } from '../modules/reviews/review.route';
 
 const router = Router();
 
@@ -21,6 +22,10 @@ const moduleRoutes = [
   {
     path: '/payment',
     route: PaymentRouter,
+  },
+  {
+    path: '/reviews',
+    route: ReviewRoutes,
   },
 ];
 
